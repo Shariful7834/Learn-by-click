82497c7d89fb276df7d9fbae0803cb26c5781732
Shariful Islam <engr.shariful.cse@gmail.com>
2022-10-11 01:48:58 +0200

    added toast for the button handler

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,23 +1,42 @@
 import React from "react";
 import "./Question.css";
 import { Button } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+import { useState } from "react";
 
 const Question = ({ singlequestion }) => {
   const { question, id, correctAnswer, options, name } = singlequestion;
+  console.log(singlequestion);
+  const [option, setOption] = useState([]);
   // console.log(singlequestion);
   // for (const option of options) {
   //   if (option === correctAnswer) {
   //     console.log("correct answer");
   //   }
   // }
+  const questionHandler = () => {
+    for (const option of options) {
+      option === correctAnswer &&
+        toast.success("Success! Right Answer", { autoClose: 500 });
+
+      // if (option === correctAnswer) {
+      // } else {
+      //   toast.error("Wrong Answer", { autoClose: 500 });
+      // }
+    }
+  };
 
   return (
     <>
-      <div className="container w-50 h-50 bg-primary">
-        <h2>{question}</h2>
+      <div className="container w-50 h-50 bg-secondary mt-5">
+        <h3>{question}</h3>
         <h3>
           {options.map((option) => (
-            <Button>{option}</Button>
+            <div className="text-center">
+              <Button onClick={questionHandler} className="my-2 w-50">
+                <li className="text-start">{option}</li>
+              </Button>
+            </div>
           ))}
         </h3>
       </div>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
+    <ToastContainer position="top-left" />
     <App />
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
