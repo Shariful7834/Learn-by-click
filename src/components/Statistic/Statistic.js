ede486e86882493d00fdf54905ab684d1fb69a24
Shariful Islam <engr.shariful.cse@gmail.com>
2022-10-12 00:09:46 +0200

    implemented the blogs page

--- src/components/Statistic/Statistic.js
+++ src/components/Statistic/Statistic.js
@@ -1,50 +1,44 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
-import './Statistic.css'
-
-
+import "./Statistic.css";
 
 const Statistic = () => {
-   const statistic = useLoaderData().data
-  //  const [name, total] = statistic
+  const statistic = useLoaderData().data;
+  const [name, total] = statistic;
 
   // console.log(name.name)
   // console.log(total.total)
-    console.log(statistic)
-
-   
+  console.log(statistic);
 
   return (
-  
-   
-      <div className="container  mt-5 ">
-     
-        <LineChart
-          width={800}
-          height={330}
-          data={statistic}
-         
-
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey='name' />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey='name' stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-
-      
-        </LineChart>
-
-  
+    <div className="container  mt-5 ">
+      <LineChart width={800} height={330} data={statistic}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="name"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+      </LineChart>
     </div>
- 
-  
-     
-  )
+  );
 };
 
 export default Statistic;
