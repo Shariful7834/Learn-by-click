2687ceb6257531d89db98a1f46ccf63ea81885a4
Shariful Islam <SIslam@isdgroup.com>
2022-10-12 11:52:43 +0200

    statistic page has responsived

--- src/components/Statistic/Statistic.js
+++ src/components/Statistic/Statistic.js
@@ -1,31 +1,26 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-import "./Statistic.css";
+import './Statistic.css'
+
+
 
 const Statistic = () => {
-  const statistic = useLoaderData().data;
-  const [name, total] = statistic;
+   const statistic = useLoaderData().data
+  //  const [name, total] = statistic
 
   // console.log(name.name)
   // console.log(total.total)
-  console.log(statistic);
+    console.log(statistic)
+
+   
 
   return (
-<<<<<<< HEAD
   
-   
-      <div className="container w-100 h-100  mt-5 ">
+   <div className="container my-5">
+    <h1>Statistic </h1>
+    <ResponsiveContainer width="95%" height={400}>
   
         <LineChart
           width={1000}
@@ -45,25 +40,11 @@ const Statistic = () => {
         </LineChart>
 
 
-=======
-    <div className="container  mt-5 ">
-      <LineChart width={800} height={330} data={statistic}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line
-          type="monotone"
-          dataKey="name"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-        <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-      </LineChart>
->>>>>>> 27218b38ebaf59c3105c467c79f2c419c4937585
-    </div>
-  );
+        </ResponsiveContainer>
+ 
+  
+        </div>
+  )
 };
 
 export default Statistic;
